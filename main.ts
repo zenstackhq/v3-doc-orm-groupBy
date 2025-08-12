990a8cecb26a3958e27ef160b64985cff81093c3
ymc9 <yiming@whimslab.io>
2025-08-12 19:01:25 +0800

    update

--- main.ts
+++ main.ts
@@ -1,8 +1,32 @@
 import { createClient } from './db';
+import { createUsersAndPosts } from './utils';
 
 async function main() {
   const db = await createClient();
+  await createUsersAndPosts(db);
 
+  console.log('Post grouped by "published"')
+  console.log(
+    await db.post.groupBy({
+      by: ['published'],
+      _count: { content: true },
+      _avg: { viewCount: true }
+    })
+  );
+
+  console.log('Post grouped by "published" and filtered for average viewCount');
+  console.log(
+    await db.post.groupBy({
+      by: ['published'],
+      _count: { content: true },
+      _avg: { viewCount: true },
+      having: {
+        viewCount: {
+          _avg: { gt: 1 }
+        }
+      }
+    })
+  );
 }
 
 main();
--- utils.ts
+++ utils.ts
@@ -4,7 +4,7 @@ import { SchemaType } from "./zenstack/schema";
 export function createPosts(db: ClientContract<SchemaType>) {
   return db.post.createMany({
     data: [ 
-      { id: 1, title: 'Post1', slug: 'post1' },
+      { id: 1, title: 'Post1', slug: 'post1', content: 'First post' },
       { id: 2, title: 'Post2', slug: 'post2', published: true, viewCount: 1 },
   ]})
 }
@@ -17,7 +17,7 @@ export async function createUsersAndPosts(db: ClientContract<SchemaType>) {
             email: 'u1@test.com',
             posts: {
                 create: [
-                    { id: 1, title: 'Post1', slug: 'post1' },
+                    { id: 1, title: 'Post1', content: 'First post', slug: 'post1' },
                 ]
             }
         }
